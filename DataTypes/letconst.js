7c6ae71fcce0a0169dd125b8a5d291ab6753dcde
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-06-03 19:58:32 +0530

    added let and const

---
+++ DataTypes/letconst.js
@@ -0,0 +1,21 @@
+let name ;
+name= "Athira";
+console.log(name);
+
+//let name = "Anna" // Reassignment is not allowed in let
+//console.log(name);
+
+name = "Aami" // updating name 
+console.log(name);
+
+//const PI = 3.14
+//PI = 5
+//console.log(PI) // TypeError: Assignment to constant variable.
+
+//Scope level access
+if(true){
+    let temp = 'notknown';
+    console.log(temp)
+}
+
+
