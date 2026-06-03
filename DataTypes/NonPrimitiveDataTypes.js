3305f9c167159b1731324fee99e99531648c6333
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-06-03 14:46:27 +0530

    updated code in nonprimitive

--- DataTypes/NonPrimitiveDataTypes.js
+++ DataTypes/NonPrimitiveDataTypes.js
@@ -1,3 +1,15 @@
 // Non -Primitive DataTypes -> Objects - Arrays, Functions...
 
+// Arrays
+let numbers = [1,2,3,4,5]
+console.log(numbers);
+
+let userData = {userName: 'Athira'}
+console.log(userData)
+//console.log(userName)// ❌ This won't work - userName doesn't exist outside the object
+console.log(userData.userName)
+console.log(typeof userData) // Type  -> object
+
+
+
 
