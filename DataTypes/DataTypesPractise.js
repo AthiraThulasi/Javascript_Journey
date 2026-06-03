f180361fb4d739eabac8b9789f6463a2a1ff7452
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-06-03 14:01:07 +0530

    practicing datatypes

---
+++ DataTypes/DataTypesPractise.js
@@ -0,0 +1,23 @@
+let name ='Athira'; // String
+let number = 8; // number
+let savings = 1000000000000n; // Big int
+let isDisciplined = true; // Boolen
+let selfDoubt = null; // Null type
+let future = undefined; // Undefined type
+let status = Symbol("unstoppable");
+
+console.log("name"); // console.log prints the value of the variable
+console.log("number");
+console.log("savings");
+console.log("isDisciplined");
+console.log("selfDoubt");
+console.log("future");
+console.log("status");
+
+console.log(typeof name) // typeOf returns the type of the variable
+console.log(typeof number)
+console.log(typeof savings)
+console.log(typeof isDisciplined)
+console.log(typeof selfDoubt)
+console.log(typeof future)
+console.log(typeof status)
