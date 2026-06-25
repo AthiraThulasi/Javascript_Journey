dc8362e22ee552580b168a155e781b39f1591699
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-06-25 22:31:19 +0530

    datatypes added

---
+++ Arrays/Array_Practice.js
@@ -0,0 +1,19 @@
+//Array in js is an object
+
+let names = [`Athira`,`Beena`,`Thulasi`]
+    console.log(names[0])
+    console.log(names[1])
+    console.log(names[2])
+
+// Traversing through array using for loop
+    for(let index = 0;index < names.length;index++){
+        console.log(names[index])
+    }
+
+    console.log(" ".repeat(20)) // for...of → gives actual array values
+    for(let x of names)
+        console.log(x)
+
+    console.log(" ".repeat(30)) // for...in → gives indices
+    for(let z in names)
+        console.log(z)
