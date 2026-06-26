2432aba6cb1e104056689d4629378e3a5ef08898
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-06-26 23:20:57 +0530

    Loops Practice

---
+++ Loops/LoopsPractice.js
@@ -0,0 +1,11 @@
+
+// Loops repeat a line of code or block of code
+
+for(let i = 0; i<=10; i++){ //code block
+    console.log(i)
+    console.log("Athira")
+}
+
+for(let i = 0; i<=10; i++) //code block
+    console.log(i)
+    console.log("Athira")
