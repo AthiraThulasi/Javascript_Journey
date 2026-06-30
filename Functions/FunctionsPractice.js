8486dc3e10184965c90a54de41f48fb851ce7753
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-06-30 23:14:43 +0530

    functions in js

--- Functions/FunctionsPractice.js
+++ Functions/FunctionsPractice.js
@@ -1,7 +1,32 @@
+
 // 1. Function Declarations
 
-function studyhard (){
+function studyhard (){ 
     console.log("Success is yours")
 }
 
-studyhard();
+studyhard(); // Function call
+
+
+// 2. Function Expression - Function assigned to a variable 
+// Anonymous function - used once or it is passed as an argument to another function
+
+const happiness = function(){ // function has no name - Anonymous Function
+    console.log("Be happy")
+}
+
+
+
+
+
+// Calling a function inside an object
+
+const user = {
+    name: "Athira",
+
+    greet: function () {
+        return `Hello ${this.name}`;
+    }
+};
+
+console.log(user.greet());
