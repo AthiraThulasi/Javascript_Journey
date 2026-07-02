1e7cad6c2658ac3315546c2035f436455433113b
Athira Beena Thulaseedharan <athira.thulasi.beena@gmail.com>
2026-07-02 23:01:15 +0530

    Array methods

--- Arrays/Array_Methods.js
+++ Arrays/Array_Methods.js
@@ -1,6 +1,19 @@
 
-// push - add values to the end of array
+let names = [ 'Athira', 'Beena', 'Thulasi']
+console.log(names.length) // prints the length
+console.log(names) // prints the elements/keys
+// stored as String in Array Objects
+
+console.log(names["0"]) // index can be passed as String // o/p-Athira
+
+console.log(names["3"]) // Access an index outside the array  returns  undefined // no Arrayindexoutofbound exception like in java
+
+
+// Add an ELEMENT in an Array
+// push - append values to the end of array
 // push returns new length of array
 
-let names = [ 'Athira', 'Beena', 'Thulasi']
-console.log(names.length)
+names.push("Neil")
+console.log(names["3"])
+names[4] = 'Athira Beena'
+console.log(names)
